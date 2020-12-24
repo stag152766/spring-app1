c19b6dca359382c6f4bd04579e2a047a327d84c8
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-12-24 17:26:59 +0700

    lesson7

--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -9,8 +9,21 @@ public class TestSpring {
                 "applicationContext.xml");
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music); // ручное создание объекта и внедрение зависимости через конструктор
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer1.playMusic();
+        boolean comparison = musicPlayer1 == musicPlayer2;
+
+
+
+        System.out.println();
+        System.out.println(comparison);
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer1.setVolume(10);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
 
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
