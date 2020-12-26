d20dae16400a551b066e5f6694beb686db18fa97
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-12-26 14:26:17 +0700

    lesson10

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,17 +1,25 @@
 package ru.alishev.springcourse;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
 
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
 
-    public void playMusic() {
-        System.out.print("Playing: " + music.getSong());
+
+    public String playMusic() {
+        return "Playing " + classicalMusic.getSong();
     }
 }
 
